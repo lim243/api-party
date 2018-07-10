0a61ef3a2cd0245056c1f89c9903b7fc5c9f5c35
Andrew Teck Kiat Lim <lim243@purdue.edu>
2018-07-10 12:02:07 -0400

    Fix bug

--- src/Github.js
+++ src/Github.js
@@ -5,53 +5,52 @@ import './Github.css'
 import GithubUser from './GithubUser'
 
 class Github extends Component {
+  state = {
+    username: '',
+  }
 
-    state = {
-        username: ''
-    }
+  handleChange = ev => {
+    this.setState({ username: ev.target.value })
+  }
 
-    handleChange = (ev) => {
-        this.setState({username : ev.target.value})
-    }
+  handleSubmit = ev => {
+    ev.preventDefault()
+    this.props.history.push(`/github/${this.state.username}`)
+  }
 
-    handleSubmit = (ev) => {
-        ev.preventDefault()
-        this.props.history.push(`/github/${this.state.username}`)
-    }
+  render() {
+    return (
+      <div className="Github">
+        <img
+          src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png"
+          alt="GitHub"
+          className="logo"
+        />
 
-    render() {
-        return (
-          <div className="Github">
-            <img
-              src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png"
-              alt="GitHub"
-              className="logo"
-            />
-    
-            <form>
-              <div>
-                <input
-                  required
-                  autoFocus
-                  type="text"
-                  value={this.state.username}
-                  onChange={this.handleChange}
-                />
-              </div>
-              <div>
-                <button type="submit">
-                  Look up GitHub user
-                </button>
-              </div>
-            </form>
-
-            <Route 
-                path="/github/:username"
-                component={GithubUser}
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <input
+              required
+              autoFocus
+              type="text"
+              value={this.state.username}
+              onChange={this.handleChange}
             />
           </div>
-        )
-      }
-    }
+          <div>
+            <button type="submit">
+              Look up GitHub user
+            </button>
+          </div>
+        </form>
+
+        <Route
+          path="/github/:username"
+          component={GithubUser}
+        />
+      </div>
+    )
+  }
+}
 
 export default Github
